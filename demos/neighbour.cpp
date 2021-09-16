ed6b9511202547060d0e7c9750ff44c22df0dc17
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2021-09-16 11:49:30 +0300

    Better illustration with 9 MPI tasks

--- demos/neighbour.cpp
+++ demos/neighbour.cpp
@@ -9,10 +9,14 @@ int main(int argc, char **argv)
     
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+    if (ntasks != 9) {
+       std::cout << "This demo works only with 9 MPI tasks" << std::endl;
+       MPI_Abort(MPI_COMM_WORLD, -1);
+    }
 
     MPI_Comm cart_comm;
     int ndims = 2;
-    int dims[2] = {2, 2};
+    int dims[2] = {3, 3};
     int periods[2] = {0, 1};
     MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, 0, &cart_comm); 
     MPI_Comm_rank(cart_comm, &rank);
@@ -33,16 +37,16 @@ int main(int argc, char **argv)
     }
 
     MPI_Barrier(cart_comm);
+    
+    int neighbors = 4;
+    std::vector<int> sendbuf(2, rank);
+    std::vector<int> recvbuf(2*neighbors, -1);
+
+    MPI_Neighbor_allgather(sendbuf.data(), 2, MPI_INT,
+                           recvbuf.data(), 2, MPI_INT,
+                           cart_comm);
 
    
-    int neighbors = 4;  
-    std::vector<int> sendbuf(2, rank); 
-    std::vector<int> recvbuf(2*neighbors, -1); 
-
-    MPI_Neighbor_allgather(sendbuf.data(), 2, MPI_INT, 
-                           recvbuf.data(), 2, MPI_INT,
-                           cart_comm); 
-
     // Print out the result
     for (int r=0; r < ntasks; r++) {
         MPI_Barrier(cart_comm);
